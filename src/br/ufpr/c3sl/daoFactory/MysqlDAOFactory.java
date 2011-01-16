d3a88aa77977ffa0459f9de2d4b87de3d3ff6346
Diego Marczal <diego@inf.ufpr.br>
2011-01-16 18:59:33 -0200

    updated jar and mysql to deploy de application

--- src/br/ufpr/c3sl/daoFactory/MysqlDAOFactory.java
+++ src/br/ufpr/c3sl/daoFactory/MysqlDAOFactory.java
@@ -13,14 +13,19 @@ import br.ufpr.c3sl.daoconcrete.MysqlUserDAO;
 
 public class MysqlDAOFactory extends DAOFactory {
 
-	private static String url = "jdbc:mysql://127.0.0.1:3306/";
-	//private static String url = "jdbc:mysql://mysql04.maxiambiental.com/";
-	//private static String database = "maxiambiental3";
-	private static String database = "carrie";
-	private static String driver = "com.mysql.jdbc.Driver";
-	private static String user = "root"; //"maxiambiental3";
-	private static String password = ""; //abc123654"; //"";
+//	private static String url = "jdbc:mysql://127.0.0.1:3306/";
+//	private static String database = "carrie";
+//	private static String driver = "com.mysql.jdbc.Driver";
+//	private static String user = "root";
+//	private static String password = "";
 
+	private static String url = "jdbc:mysql://mysql04.maxiambiental.com/";
+	private static String database = "maxiambiental3";
+	private static String driver = "com.mysql.jdbc.Driver";
+	private static String user = "maxiambiental3";
+	private static String password = "abc123654";
+	
+	
 	// TODO: Recommend connection pool implementation/usage
 	public static Connection createConnection() {   
 		try {
