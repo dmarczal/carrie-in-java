b2010e142c3fa46bb7d1c3ad1c5e3a26f25a9dff
Diego Marczal <diego@inf.ufpr.br>
2011-01-24 15:40:11 -0200

    changed deploy file

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,12 @@ scp fractal_retro.tar.gz diego@ssh.c3sl.ufpr.br:
 
 rm fractal_retro.tar.gz
 
+if ! [ -z $1 ]; then
+  if [ $1 = "uca" ]; then 
+	ssh diego@ssh.c3sl.ufpr.br "scp fractal_retro.tar.gz condigital@cognac:www/fractal_retro_uca && ssh condigital@cognac 'cd www/fractal_retro_uca && rm index.html && rm -rf jars/ && tar -xvzf fractal_retro.tar.gz && rm fractal_retro.tar.gz && mv -f html/* . && rm -rf html/'"
+  fi
+else
+
 ssh diego@ssh.c3sl.ufpr.br "scp fractal_retro.tar.gz condigital@cognac:www/fractal_retro && ssh condigital@cognac 'cd www/fractal_retro && rm index.html && rm -rf jars/ && tar -xvzf fractal_retro.tar.gz && rm fractal_retro.tar.gz && mv -f html/* . && rm -rf html/'"
+
+fi
